6c24f7ec2420ad948da5129ecc6dae06be31ce84
Bruno Lorenzo <b.lorenzo@houlak.com>
2020-08-02 22:56:14 -0300

    Code refactor to make all example animations enable

--- api-animation/ViewController.swift
+++ api-animation/ViewController.swift
@@ -15,8 +15,16 @@ class ViewController: UIViewController {
         let button = UIButton()
         button.setTitle("Animate!", for: .normal)
         button.addTarget(self, action: #selector(animate), for: .touchUpInside)
-        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 12
         return button
+    }()
+    
+    private lazy var segmentedBar: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["Fade", "Scale & Fade", "Bounce", "Scale & Bounce"])
+        segmentedControl.selectedSegmentIndex = 0
+        return segmentedControl
     }()
     
     override func viewDidLoad() {
@@ -30,9 +38,15 @@ class ViewController: UIViewController {
         squareView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
+        view.addSubview(segmentedBar)
+        segmentedBar.translatesAutoresizingMaskIntoConstraints = false
+        segmentedBar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        segmentedBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        segmentedBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+        
         view.addSubview(startAnimationButton)
         startAnimationButton.translatesAutoresizingMaskIntoConstraints = false
-        startAnimationButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        startAnimationButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
         startAnimationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         startAnimationButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
     }
@@ -50,23 +64,30 @@ extension ViewController {
             view.alpha = 1
         }
         
-//        squareView.performeAnimation(.fade(.fadeIn, withDuration: 2, onCompletion: completionHandler))
+        switch segmentedBar.selectedSegmentIndex {
+        case 0:
+            squareView.performeAnimation(.fade(.fadeIn, withDuration: 2, onCompletion: completionHandler))
+        case 1:
+            squareView.performeAnimations(.inParallel,
+                with:
+                .scale(.scaleIn, withDuration: 1.5, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
+                .fade(.fadeIn, withDuration: 3.0, onCompletion: completionHandler))
+        case 2:
+            squareView.performeAnimations(.inSequence,
+                with:
+                .bounce(.bounceOut, withDuration: 2.0, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
+                .fade(.fadeOut, withDuration: 1.0, onCompletion: completionHandler))
+        case 3:
+            squareView.performeAnimations(.inSequence,
+                with:
+                .scale(.scaleOut, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
+                .bounce(.bounceOut, withDuration: 1, dumpingRatio: 0.3, scaleFactor: CustomAnimation.ScaleFactor(x: 0.3, y: 0.3)),
+                .fade(.fadeOut, withDuration: 0.3, onCompletion: completionHandler))
+        default:
+            break
+        }
+
         
-//        squareView.performeAnimations(.inParallel,
-//            with:
-//            .scale(.scaleIn, withDuration: 1.5, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
-//            .fade(.fadeIn, withDuration: 3.0, onCompletion: completionHandler))
         
-//        squareView.performeAnimations(.inSequence,
-//            with:
-//            .bounce(.bounceOut, withDuration: 2.0, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
-//            .fade(.fadeOut, withDuration: 1.0, onCompletion: completionHandler))
-        
-        squareView.performeAnimations(.inSequence,
-            with:
-            .scale(.scaleOut, scaleFactor: CustomAnimation.ScaleFactor(x: 1.3, y: 1.3)),
-            .bounce(.bounceOut, withDuration: 1, dumpingRatio: 0.3, scaleFactor: CustomAnimation.ScaleFactor(x: 0.3, y: 0.3)),
-            .fade(.fadeOut, withDuration: 0.3, onCompletion: completionHandler)
-        )
     }
 }
